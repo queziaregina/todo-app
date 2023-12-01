cbaa865ff380a80c99ba7b89311329d29f21db36
Quezia Regina <queziaregina32@gmail.com>
2023-11-30 23:02:24 -0300

    deixando a mudança de tema dinâmica

---
+++ public/js/script.js
@@ -0,0 +1,43 @@
+function alterarTema () {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if (tema) {
+        let novoTema
+
+        if (tema === "light") {
+            novoTema = "dark"
+            button.innerHTML = `<img src="/imagens/sun-icon.png" alt="ícone de Sol"></img>`
+            body.classList.remove("light")
+            body.classList.add("dark")
+        } else {
+            novoTema = "light"
+            button.innerHTML = `<img src="/imagens/moon-icon.png" alt="ícone de Lua"></img>`
+            body.classList.remove("dark")
+            body.classList.add("light")
+        }
+
+        localStorage.setItem("tema", novoTema)
+        return
+    }
+
+    localStorage.setItem("tema", "dark")
+    body.classList.add("dark")
+}
+
+function verificarTema() {
+    const tema = localStorage.getItem("tema")
+    const body = document.querySelector("body")
+    const button = document.querySelector(".tema-button")
+
+    if(tema === "dark") {
+       body.classList.add("dark") 
+       button.innerHTML = `<img src="/imagens/sun-icon.png" alt="ícone de Sol"></img>`
+    } else {
+        body.classList.add("light")
+        button.innerHTML = `<img src="/imagens/moon-icon.png" alt="ícone de Lua"></img>`
+    }
+}
+
+verificarTema()
